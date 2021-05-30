5fbc565f6ebb478874cf001cc4c71e3f919f4465
Kho Yee, Choy <khoyee@gmail.com>
2021-05-30 17:25:24 +0900

    use font that supports Japanese

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=24:antialias=true:autohint=true" };
+static const char *fonts[]          = { "NotoSansMono Nerd Font:pixelsize=24:antialias=true:autohint=true", "JoyPixels:pixelsize=24:antialias=true:autohint=true", "monospace:size=10"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
