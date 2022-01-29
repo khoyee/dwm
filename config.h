769a1ed1bd6c847742c4688e293dd962be6094db
Kho Yee, Choy <khoyee@dotkoyi.me>
2022-01-29 22:29:30 +0900

    change default web browser to firefox

--- config.h
+++ config.h
@@ -99,7 +99,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *webbrowsercmd[] = { "google-chrome-stable", "--force-device-scale-factor=1.5", NULL };
+static const char *webbrowsercmd[] = { "firefox", NULL };
 static const char *filemancmd[]  = { "st", "-e", "ranger", NULL };
 static const char *editorcmd[]  = { "subl", NULL };
 
